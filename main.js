08a739d7fb57bad70e50e7e17da90f3206ff014c
vits03 <101546379+vits03@users.noreply.github.com>
2022-06-18 13:56:27 +0400

    implemented part of the user interface.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const container =document.querySelector(".grid-container");
 console.log(container);
 
+
 let id =1;
 let ROW=25;
 let COLUMN=25;
@@ -16,26 +17,60 @@ for (let j=0;j<ROW;j++){
     }
     container.appendChild(row);
 }
+let toggle=false;
+let color="black";
+let draw=false;
+let rainbow=["#ff0000","#ffa500","#ffff00","#008000","#0000ff","#4b0082","#ee82ee"];
+let r=0;
 
-let draw=0;
+const get_color=()=>{
+  r++
+  if( r>=8) {
+    r=0;
+  } 
+  return rainbow[r];
+}
 for (let n=1;n<=(ROW*COLUMN);n++){
        
        const square=document.querySelector(`.grid[id="${n}"]`);
        container.addEventListener('mousedown',(e) => {
-        console.log("fsf");draw=1;});
+        console.log("fsf");draw=true;});
        
         container.addEventListener('mouseup',(e) => {
-            console.log("fsf");draw=0;});
+            console.log("fsf");draw=false;});
         console.log(draw);
        
        
         
       
         console.log(n);
-        square.addEventListener('mousemove',(e) =>{
-             if (draw==1){
+        square.addEventListener('mousemove',(e) =>{   
+            if (draw){
+              if (toggle){
+                square.style.backgroundColor=get_color();
+                
+              }
+              else {
              console.log(" button clicked");
-             square.style.backgroundColor="red";}
+             square.style.backgroundColor=color;}}
       } );
     }
+let clear=document.querySelector(".clear");
+let rainbowbtn=document.querySelector(".rainbow");
+let logo =document.getElementById("logo");
+let eraser=document.querySelector('.eraser');
+console.log(logo);
+rainbowbtn.addEventListener('click',() => {toggle=true;});
+col=document.getElementById('colorpicker');
+eraser.addEventListener('click',()=>  {toggle=false;color='white'})
+clear.addEventListener('click',() => {clear_all();});
+col.addEventListener('input',()=>{toggle=false;color=col.value;logo.style.color=col.value;});
+col.addEventListener('click',()=>{toggle=false;color=col.value;logo.style.color=col.value;});
 
+function clear_all() {
+  for (let n=1;n<=(ROW*COLUMN);n++){
+       
+    const square=document.querySelector(`.grid[id="${n}"]`);
+    square.style.backgroundColor="white";
+}
+}
