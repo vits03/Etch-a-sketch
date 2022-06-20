51a29664db6cd80879c300f74e4616317f76c6e1
vits03 <101546379+vits03@users.noreply.github.com>
2022-06-20 14:19:22 +0400

    added the "secret button" and selected button now changes when active.

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 const container =document.querySelector(".grid-container");
 console.log(container);
 
+let secret=document.querySelector('.secret-link')
+let clear=document.querySelector(".clear");
+let rainbowbtn=document.querySelector(".rainbow");
+let logo =document.getElementById("logo");
+let eraser=document.querySelector('.eraser');
+let colorbtn=document.querySelector('.color-btn')
 
 let id =1;
 let ROW=25;
 let COLUMN=25;
 for (let j=0;j<ROW;j++){
     const row = document.createElement('div');
+    row.setAttribute('draggable','false')
     row.classList.add('row');
       for (let i=0;i<COLUMN;i++){
          const div = document.createElement('div');
+         div.setAttribute('draggable', 'false');
          div.classList.add('grid');
          div.setAttribute('id',id)
           row.appendChild(div);
@@ -51,30 +59,36 @@ for (let n=1;n<=(ROW*COLUMN);n++){
                 
               }
               else {
-             console.log(" button clicked");
+             
              square.style.backgroundColor=color;}}
       } );
     }
-    
-let secret=document.querySelector('.secret-link')
-let clear=document.querySelector(".clear");
-let rainbowbtn=document.querySelector(".rainbow");
-let logo =document.getElementById("logo");
-let eraser=document.querySelector('.eraser');
-let colorbtn=document.querySelector('.color-btn')
-console.log(logo);
-rainbowbtn.addEventListener('click',() => {toggle=true;});
+select(colorbtn);    
+
+rainbowbtn.addEventListener('click',() => {toggle=true;select(rainbowbtn);});
 col=document.getElementById('colorpicker');
-eraser.addEventListener('click',()=>  {toggle=false;color='white'})
-clear.addEventListener('click',() => {clear_all();});
-col.addEventListener('input',()=>{toggle=false;color=col.value;logo.style.color=col.value;});
-col.addEventListener('click',()=>{toggle=false;color=col.value;logo.style.color=col.value;});
-colorbtn.addEventListener('click',() => {toggle=false;color=col.value;})
+eraser.addEventListener('click',()=>  {toggle=false;color='white';select(eraser)})
+clear.addEventListener('click',() => {clear_all();select(clear)});
+col.addEventListener('input',()=>{toggle=false;color=col.value;logo.style.color=col.value;select(colorbtn)});
+col.addEventListener('click',()=>{toggle=false;color=col.value;logo.style.color=col.value;select(colorbtn)});
+colorbtn.addEventListener('click',() => {toggle=false;color=col.value;select(colorbtn);})
 secret.addEventListener('click',()=>{secret.setAttribute("href","https://www.youtube.com/watch?v=dQw4w9WgXcQ")});
+
+
 function clear_all() {
   for (let n=1;n<=(ROW*COLUMN);n++){
        
     const square=document.querySelector(`.grid[id="${n}"]`);
     square.style.backgroundColor="white";
 }
+}
+
+function select(obj){
+  let buttons=document.querySelectorAll('#btn');
+  console.log(buttons);
+  buttons.forEach(e=>{e.className=".buttons button"})
+  if (obj.innerText==="CLEAR") {
+      return;
+  }
+  obj.className="btn";
 }
